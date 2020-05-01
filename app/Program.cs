a38f5fca37474f667f2edc98f9f963e11c54c93a
Jonathan Melly <jonathan.melly@gmail.com>
2020-05-01 18:07:31 +0200

    Fixes #52 #53 task task task *blablalbalbal
    Took 3 minutes

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,7 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("He llo World!");
             Console.WriteLine("Fixes   AB#4   9 ");
             Console.WriteLine("Fixes AB #49   ");
             Console.WriteLine("Fixes AB#49   ");
