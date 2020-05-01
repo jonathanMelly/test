67a2170c3d4517b059c0ccf44e9492126ea10671
Jonathan Melly <jonathan.melly@gmail.com>
2020-05-01 15:38:37 +0200

    Test #50 ref Test AB#49 ref Test #51 ref Fixes #4 (test gh)
    Took 3 minutes

--- app/Program.cs
+++ app/Program.cs
@@ -9,6 +9,7 @@ namespace app
             Console.WriteLine("Hello World!");
             Console.WriteLine("Fixes AB#49 ");
             Console.WriteLine("Fixes AB#49   ");
+            Console.WriteLine("Fixes AB#49   ");
 
         }
     }
