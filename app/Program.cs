cde6f0a02140751f672e5df2a66b7d697e5d1b3a
Jonathan Melly <jonathan.melly@gmail.com>
2020-05-02 15:51:36 +0200

    Test #50 ref Test AB#49 ref Test #51 ref Fixes #4 (test gh) Fixes #52 #53 task task task *blablalbalbal
    Took 3 minutes

--- app/Program.cs
+++ app/Program.cs
@@ -7,7 +7,7 @@ namespace app
         static void Main(string[] args)
         {
             Console.WriteLine("He llo World!");
-            Console.WriteLine("Fixes   AB#4   9 ");
+            Console.WriteLine("Fixes    AB#4   9 ");
             Console.WriteLine("Fixes AB #49   ");
             Console.WriteLine("Fixes AB#49   ");
 
